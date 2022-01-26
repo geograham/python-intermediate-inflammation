e1f165e7adf6b7224ecf6c1f8b1d829b481c047f
ggraham <georgina.graham@ukaea.uk>
2022-01-26 16:08:24 +0000

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,55 +2,55 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ])
+
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[1, 2, 3], [3, 4, 5], [5, 6, 7]], [5, 6, 7]),
+        ([[1, 2, -3], [3, -4, -5], [-5, -6, -7]], [3, 2, -3]),
+    ])
 
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
+def test_daily_max(test, expected):
+    """Test max function works for array of zeroes, positive integers, and positive and negative integers."""
+    from inflammation.models import daily_max
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
-def test_daily_min():
-    """Test that min function works for an array of positive and negative integers."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[1, 2, 3], [3, 4, 5], [5, 6, 7]], [1, 2, 3]),
+        ([[1, 2, -3], [3, -4, -5], [-5, -6, -7]], [-5, -6, -7]),
+    ])
+
+def test_daily_min(test, expected):
+    """Test min function works for array of zeroes, positive integers, and positive and negative integers."""
+    from inflammation.models import daily_min
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
     from inflammation.models import daily_min
 
-    test_input = np.array([[1, 2, -2],
-                           [-3, -4, -4],
-                           [5, -6, -6]])
-    test_result = np.array([-3, -6, -6])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
-
-def test_daily_max():
-    """Test that max function works for an array of positive and negative integers."""
-    from inflammation.models import daily_max
-
-    test_input = np.array([[1, 1, 2, -2],
-                           [3, -3, -4, -4],
-                           [5, 5, -6, -6]])
-    test_result = np.array([5, 5, 2, -2])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
 # TODO(lesson-robust) Implement tests for the other statistical functions
